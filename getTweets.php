806dfd2090b9a17c33221fd3c0888ba14efed46b
Marc <marc.pous@gmail.com>
2012-05-26 15:24:47 +0300

    Update getTweets.php

--- getTweets.php
+++ getTweets.php
@@ -5,20 +5,16 @@
 
 	* Query for tweets doing checking to 4sq service 20 miles from the center of Barcelona
 	* Store the tweets in a JSON file to analyze them later
-*/
+*/  
 
+ // Tweets  20 miles around Barcelona with 4sq.com links inside
  $q = "http://search.twitter.com/search.json?q=4sq.com&include_entities=true&result_type=recent&geocode=41.38615876984,2.1710175275803,20mi";
-
- print_r($q);
- echo "<hr>";
 
  try {
    // Make call with cURL
    $session = curl_init($q);
    curl_setopt($session, CURLOPT_RETURNTRANSFER,true);
    $json = curl_exec($session);
-
-   print_r($json);
 
    } catch (Exception $e) {
 	print_r($e);
